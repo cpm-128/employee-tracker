902cd172f9f48e7ee2b3530c6355525748117310
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-06-30 16:00:13 -0400

    modularized code base; creased routes directory and updated server.js to reference

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,10 @@
+// this is a central hub for all apiRoutes
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./departmentsRoutes'));
+//router.use(require('./rolesRoutes'));
+//router.use(require('./employeesRoutes'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./db/connection');
-//const apiRoutes = require('./routes/apiRoutes');
-const inputCheck = require('./utils/inputCheck');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,54 +10,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // API ROUTES (endpoints)
-//app.use('/api', apiRoutes);
-
-// GET all department names and department ids
-app.get('/api/departments', (req, res) => {
-    const sql = `SELECT * FROM departments`;
-
-    db.query(sql, (err, rows) => {
-        if (err) {
-            res.status(500).json({ error: err.message });
-            return;
-        }
-        res.json({
-            message: 'success',
-            data: rows
-        });
-    });
-});
-
-// POST a department
-// assume object req.body will be used to populate the candidate's data
-app.post('/api/departments', ({ body }, res) => {
-    // data validatation first
-    const errors = inputCheck(body, 'name');
-    if (errors) {
-        res.status(400).json({ errors: errors });
-        return;
-    }
-
-    const sql = `INSERT INTO departments (name) VALUES (?)`;
-    const params = [body.name];
-
-    db.query(sql, params, (err, result) => {
-        if (err) {
-            res.status(400).json({ error: err.message });
-        }
-        res.json({
-            message: 'success',
-            data: body
-        });
-    });
-});
+app.use('/api', apiRoutes);
 
 // TEST CONNECTION
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello, World!'
-    });
-});
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello, World!'
+//     });
+// });
 
 // Default response for any requests that are Not Found
 app.use((req, res) => {
